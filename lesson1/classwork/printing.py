74364610d422fce063fed46848f013d1a0bb383d
saarna1720-lab <saarna1720@gmail.com>
2026-05-10 01:01:18 +0000

    Finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("My name is Aarna")
+print('My favorite food is sushi')
+
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,9 @@
+x=5 # integer: whole number
+print(x)
+
+number = 6.7 # float: number with a decimal point
+print(number)
+
+favorite_food = "sushi" # string: text enclosed in quotes
+print("My favorite food is", favorite_food)
+print(4.1,"dogs",x)
